13da4236cd88a46eca12058a10a0573530ebde6b
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-09 12:04:21 +0200

    #5 - layout a logika

--- MyApp005ToastSnackbar/app/src/main/java/com/example/myapp005toastsnackbar/MainActivity.kt
+++ MyApp005ToastSnackbar/app/src/main/java/com/example/myapp005toastsnackbar/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.myapp005toastsnackbar
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp005toastsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Nastavení akce pro tlačítko Zobrazit TOAST
+        binding.btnShowToast.setOnClickListener {
+            val toast = Toast.makeText(this, "Nazdar - MÁM HLAD", Toast.LENGTH_LONG)
+            toast.show()
         }
+
+        // Nastavení akce pro tlačítko Zobrazit SNACKBAR
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Jsem Snackbar = jsem víc než TOAST", Snackbar.LENGTH_LONG)
+                .setBackgroundTint(Color.parseColor("#FFCC50"))
+                .setTextColor(Color.BLACK)
+                .setDuration(7000)
+                .setActionTextColor(Color.WHITE)
+                .setAction("Zavřít") {
+                    Toast.makeText(this, "Zavírám SNACKBAR", Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        }
+
+
+
+
     }
 }
