0a35fc713bf641c1967622f48c69a23ac6af8820
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 15:22:15 +0200

    #8 SharedPref - ukládání a načtení dat

--- MyApp008aSharedPreferences/app/src/main/java/com/example/myapp008asharedpreferences/MainActivity.kt
+++ MyApp008aSharedPreferences/app/src/main/java/com/example/myapp008asharedpreferences/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapp008asharedpreferences
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,7 +19,45 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        // Přístup k SharedPreferences
+        val sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        // Funkcionalita pro ukládání dat
+        binding.btnSave.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val ageString = binding.etAge.text.toString().trim()
+
+            if(ageString.isBlank()) {
+                Toast.makeText(this, "Musíš vyplnit věk...", Toast.LENGTH_LONG).show()
+            } else {
+                val age = ageString.toInt()
+                val isAdult = binding.cbAdult.isChecked
+                if((age < 18 && isAdult) || (age >= 18 && !isAdult)) {
+                    Toast.makeText(this, "Kecáš, takže nic ukládat nebudu...", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Ukládám data...", Toast.LENGTH_LONG).show()
+                    editor.apply {
+                        putString("name", name)
+                        putInt("age", age)
+                        putBoolean("isAdult", isAdult)
+                        apply()
+                    }
+                }
+            }
+        }
+
+
+        // Funkcionalita pro načtení dat
+        binding.btnLoad.setOnClickListener {
+            val name = sharedPref.getString("name", null)
+            val age = sharedPref.getInt("age", 0)
+            val isAdult = sharedPref.getBoolean("isAdult", false)
+
+            binding.etName.setText(name)
+            binding.etName.setText(age.toString())
+            binding.cbAdult.isChecked = isAdult
+        }
 
     }
 }
