c453b6557aa7112cf4eccfd7055fc4a92617bb02
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 11:24:17 +0200

    #7 úpravy layoutu pro fragmenty a logika pro fragmenty

--- MyApp007aFragments/app/src/main/java/com/example/myapp007afragments/MainActivity.kt
+++ MyApp007aFragments/app/src/main/java/com/example/myapp007afragments/MainActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp007afragments.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
         }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        // získání instance správce fragmentu
+        val fragmentManager = supportFragmentManager
+
+        // vyvoření nové transakce pro fragmenty
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        // nahrazení fragmentu ve view kontejneru novým fragmentem, který byl předán jako argument
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+
+        // potvrzení transakce a provedení výměny fragmentů
+        fragmentTransaction.commit()
     }
 }
