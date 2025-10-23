40f91ef3b145fb3f443751d2c82451e8d16318b2
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-23 11:20:45 +0200

    #9 ImageToApp - logika a mala uprava layoutu

--- MyApp009aImageToApp/app/src/main/java/com/example/myapp009aimagetoapp/MainActivity.kt
+++ MyApp009aImageToApp/app/src/main/java/com/example/myapp009aimagetoapp/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.myapp009aimagetoapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp009aimagetoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            uri: Uri? -> binding.ivImage.setImageURI(uri)
+        }
+
+        binding.btnTakeImage.setOnClickListener {
+            // Tento příkaz spustí výběr obrázku z telefonu
+            // Vybraný obrázek se zobrazí v ImageView
+            getContent.launch("image/*")
         }
     }
 }
