b918346ded5da8cb393370a878abc20a819f5ad0
Tomáš Žižka <tomas.zizka@tul.cz>
2025-11-06 11:37:18 +0100

    #12 Poznamkovnik - abstract class for Database - NoteHubDatabase

---
+++ MyApp012aMyNoteHub/app/src/main/java/com/example/myapp012amynotehub/data/NoteHubDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.myapp012amynotehub.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import java.util.Locale
+
+@Database(
+    entities = [Note::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class NoteHubDatabase: RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+}
