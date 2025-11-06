c39c63f353511b11b83f519877cf604989563021
Tomáš Žižka <tomas.zizka@tul.cz>
2025-11-06 11:11:49 +0100

    #12 Poznamkovnik - příprava pro ROOM v gradle souborech...

--- MyApp012aMyNoteHub/app/build.gradle.kts
+++ MyApp012aMyNoteHub/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,6 +42,11 @@ android {
 }
 
 dependencies {
+    val room_version = "2.8.3"
+    implementation("androidx.room:room-runtime:${room_version}")
+
+    ksp("androidx.room:room-compiler:$room_version")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- MyApp012aMyNoteHub/build.gradle.kts
+++ MyApp012aMyNoteHub/build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    //id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
 }
