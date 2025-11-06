3fe8a433c47c95685a6688ad1111f09a3b393ee1
Tomáš Žižka <tomas.zizka@tul.cz>
2025-11-06 11:22:23 +0100

    #12 Poznamkovnik - Entity - Note

---
+++ MyApp012aMyNoteHub/app/src/main/java/com/example/myapp012amynotehub/data/Note.kt
@@ -0,0 +1,11 @@
+package com.example.myapp012amynotehub.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,  // ID poznámky, automaticky generované
+    val title: String, // Název poznámky
+    val content: String // Obsah poznámky
+)
